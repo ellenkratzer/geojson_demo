3939a49969c735a6e69364e98bc7e65daf9c5061
ellenkratzer <72271170+ellenkratzer@users.noreply.github.com>
2020-10-02 11:29:14 -0500

    Update geojson

--- geojson.js
+++ geojson.js
@@ -12,166 +12,102 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#555555",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#555555",
-        "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#ee05ff",
+        "marker-color": "#b75c96",
         "marker-size": "medium",
         "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "title": "Holmes Lake",
+        "image": "https://d1gtgy3y5x24xv.cloudfront.net/f01f-32640467-Holmes-Lake_75ec876f-5056-a36a-07f07cafb73b21cc.jpg?versionId=XSb_uW6Lb1tbbg8fagK71HaLw1IWKTwZ"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
+          -96.63402557373047,
+          40.77755171188342
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
+        "marker-color": "#b58cba",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "The Mill @ Telegraph",
+        "image": "https://lh3.googleusercontent.com/dZ9iS8nQlAPNnF0sQZJ7fViRngTS93xlnRk2Z4jYyhffu-cLVJnKlKOQBQo_cz60iaYbtxFinGOxxCyxb4MraA-wC75gX1LjSuvt0ntwUp-IIRB25fFSattpzQjgw17-vWDpSw3t1f09WQCraq-VYtSMhgVYdZB_I36jg82p5YF4Wz9Nb79hC9PM3bS2TcWPFpkqvci0eSqgrEU1mTjxeFjxkNwJXbnpKspEPIVfbATZtE4dI4HTCCxu6qEs5NbITeZwsk8J8yRHd8BtZtMj6PpEbTdwZ8gQJDCaguvev63lRmEuiwDt193yyCuAThXb_nFOLLnBn85oXwFxhl16v_zqoxffeFVryQwehH4aY6c-la7-OmqijbUrybgU3kllZLAqwcfgkvX92sBSLufP53NWZldJ9eCnockkvyKgyY2jkngCt8GnYdCdUelC25On4MiS8qMlxNutt1fV5459xQ1v4--ljETeyWyUcewVUjbCXpglolFSjY5fYlTt_AKujtA235W866Sqd88WiWSi4mXTnpPvYpul5kLH_uwkPHYbr-xS5DIX-Hlulq8u6Dje6_6IGEGcg6L2VqBbh4rgsjVfkXjSfKqtFLSWIW92zs29JEOZVyQWnZ0uLj4Af6MLzT2wkEu93FXC1cc316dvT0iKlmqBjGFCWYFUN_PmJXtQoOHRwTokyOMS7Jka=w745-h993-no?authuser=0"
       },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
-          ]
+          -96.69028759002686,
+          40.81037438759006
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
+        "marker-color": "#8ba5d0",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Sheldon Art Museum",
+        "image": "https://sheldonartmuseum.org/images/Architecture/SMA_Main-Gallery-34_800px.jpg"
       },
       "geometry": {
-        "type": "Polygon",
+        "type": "Point",
         "coordinates": [
-          [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ]
-          ]
+          -96.70438528060913,
+          40.81753619944697
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#db00ac",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "The Happy Raven",
+        "image": "https://lh3.googleusercontent.com/oLLG7aDrWZuYGwJyHAg8xo47c8ieqEaSDlLeHJf6Y4HcSDVF7hHbY2Sr2NmQyc0L8aCSyo-PYI5qEBDXpxb9163xCTduzkAsERnPyonsHcUclZLuhif6EThUPJvPsJxVVeJDqsbPfjNPTCMc6WwFV49hmdRdPb33eSMR9M-uw7pLPZ1v4dWIFCc3lsRT77Kl01t8jdccPLl040zwXJWaqIck6HKSJknRV_kCH6a7AmePvItG09voCR6DiJvI4dde299WHrU1hDVCdl9g1ZV57lxWg9Sgdayw1EVQ02d5B1LC19NCG47htakUM8OWzBGrnuf70EbbF65j-OCUDDRG1LuCXgyN0O_dD_Wj-NZXBdOC5Wi12cjI8rgleV0i6SN8xEnQDRRnCHeMpoyXyQMUvOryqCm6LUYkge_JwsYt2G9z1X0N7VLizjq494d70A8SgAQdfSlFwEJ6IpZoRgfOf4Et0NRxUHVjNzLasPrkTGy_ngJdT65t0sm4ueKtN7i-YkAhySzKLnxUwLAKH56fE3ITL9cFBPNJb2801kkU05-i68XUNnInaX5Esn0hobAhah5uaETYP3zWXw0d8-ZyPSWtAqW_zAtokg_Xn8Y58K62O8Bm5zEwFUQBHXlrIoCav4YucJ_z4r9IMkaN5I7MsWQOlPIVxvsh6rGzt9zFjHUnEHkfmwqPIgdCwfYo=w745-h993-no?authuser=0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70529186725616,
+          40.81402440984917
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#94c2d6",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "The Rabbit Hole Bakery",
+        "image": "https://downtownlincoln.org/_files/images/rabbithole_outsidesm.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70975908637045,
+          40.816039144243945
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#a555af",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Alchemy",
+        "image": "https://downtownlincoln.org/_files/images/alchemy_inside.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70980602502823,
+          40.8144263466341
         ]
       }
     }
